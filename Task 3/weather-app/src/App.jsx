20535ccda595ad2fe49ea5a632db351e1165e3a7
Saad Khan <saadnadeem885@gmail.com>
2023-09-21 23:15:42 +0500

    new commit

--- Task 3/weather-app/src/App.jsx
+++ Task 3/weather-app/src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+  // 09386c9fb35b3d7b3b6c888674a466a2
   const [input, setInput] = useState()
-  const search = (val) => {
-    setInput(val.target.value)
+  const [key, setKey] = useState('09386c9fb35b3d7b3b6c888674a466a2')
+  const [weather, setWeather] = useState({
+    name: '',
+    temp: 0,
+    humidity: 0,
+    windSpeed: 0,
+    pressure: 0,
+    visibility: 0
+  })
+
+  const handleClick = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&units=Metric&appid=${key}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setWeather({...weather, name: data.name, temp: data.main.temp, humidity: data.main.humidity, windSpeed: data.wind.speed, pressure: data.main.pressure, visibility: data.visibility})
+      })
+      .catch((err) => {
+        setWeather({err: err})
+      })
   }
+
+
   return (
     <div className='flex justify-center items-center h-screen'>
       <img className='absolute inset-0 h-full w-full object-cover' src="./images/cloudy.jpg" alt="" />
@@ -18,48 +40,48 @@ function App() {
               type='search'
               id='search-field'
               placeholder='Searh here'
-              onChange={search}
+              onChange={e=> setInput(e.target.value)}
             />
           </div>
-          <div className='p-2 cursor-pointer'>
+          <button onClick={handleClick} className='p-2 cursor-pointer'>
             <img src='./images/search.png' alt='' height={25} width={25} />
-          </div>
+          </button>
         </div>
         <div className='flex flex-col justify-center items-center'>
           <p className='font-sans text-4xl font-semibold mt-8'>
-            New York
+            {weather && weather.name}
           </p>
           <div className='flex items-center mt-2'>
             <img src='./images/cloudy.png' alt='' width={60} height={60} />
-            <p className='text-5xl ml-4 text-blue-500'>20&deg; C</p>
+            <p className='text-5xl ml-4 text-blue-500'>{weather && weather.temp}&deg; C</p>
           </div>
         </div>
         <div className='mt-16 mob-display:mt-8 flex justify-evenly mob-display:flex-col w-full'>
           <div className='flex mob-display:flex-row flex-col items-center shrink-0 mob-display:justify-between p-6  rounded-2xl'>
             <img src='./images/humidity.png' alt='' height={40} width={50} />
             <div className='flex flex-col items-center text-lg font-semibold ml-4'>
-              <p>64%</p>
+              <p>{weather && weather.humidity}</p>
               <p>Humidity</p>
             </div>
           </div>
           <div className='flex mob-display:flex-row flex-col items-center shrink-0 mob-display:justify-between mob-display:mt-4 p-6  rounded-2xl'>
             <img src='./images/wind.png' alt='' height={40} width={50} />
             <div className='flex flex-col items-center text-lg font-semibold ml-4'>
-              <p>18 Km/Hr</p>
+              <p>{weather && weather.windSpeed}</p>
               <p>Wind Speed</p>
             </div>
           </div>
           <div className='flex mob-display:flex-row flex-col items-center shrink-0 mob-display:justify-between mob-display:mt-4 p-6  rounded-2xl'>
             <img src='./images/pressure.png' alt='' height={40} width={50} />
             <div className='flex flex-col items-center text-lg font-semibold ml-4'>
-              <p>1015</p>
+              <p>{weather && weather.pressure}</p>
               <p>Pressure</p>
             </div>
           </div>
           <div className='flex mob-display:flex-row flex-col items-center shrink-0 mob-display:justify-between mob-display:mt-4 p-6  rounded-2xl'>
             <img src='./images/visibility.png' alt='' height={40} width={50} />
             <div className='flex flex-col items-center text-lg font-semibold ml-4'>
-              <p>10000</p>
+              <p>{weather && weather.visibility}</p>
               <p>Visibility</p>
             </div>
           </div>
